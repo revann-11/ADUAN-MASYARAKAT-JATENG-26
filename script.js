83688abaa5b00de711f4feda729c11c8ea14a149
revann-11 <revanganteng1103@gmail.com>
2026-04-30 21:58:37 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("formAduan").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    document.getElementById("formAduan").style.display = "none";
-    document.getElementById("successMsg").style.display = "block";
-});
